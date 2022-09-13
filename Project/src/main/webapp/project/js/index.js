416eb1ce4cbca0dbea77a327806c4b157c0280aa
zzangyu <hellohan95@gmail.com>
2022-09-13 09:02:10 +0900

    index.jsp 수정

--- Project/src/main/webapp/project/js/index.js
+++ Project/src/main/webapp/project/js/index.js
@@ -2,9 +2,9 @@ var ViewLayer = function(cityA) {
     var city = cityA;
     var country = document.getElementsByClassName('photo');
     for (var i =0; i<country.length;i++) {
-       	if(country[i].style.display === 'block') {
+       	if(country[i].style.display === 'inline-block') {
     		country[i].style.display = 'none';
     	}
     }
-	document.getElementById(city).style.display = 'block';
+	document.getElementById(city).style.display = 'inline-block';
 }
